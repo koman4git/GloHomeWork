782e8b33bd2abbd80513cca881ba43c15cbd816e
Andrey Lazarew <koman4bk@bk.ru>
2022-01-29 11:27:51 +0300

    переменным присвоены новые значения, вывод данных и их типов в консоль

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,18 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "Первый проект";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 15000;
+let rollback = Math.ceil(Math.random()*100);
+let fullPrice = 100000;
+let adaptive = true;
 
-alert('Первое домашнее задание!)');
-console.log('Проверка подключения файла');
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " рублей" + "(" + Math.round(screenPrice/77.82) + " долларов" + " или " + Math.round(screenPrice/2.7) + " гривен" + " или " + Math.round(screenPrice/12.23) + " юаней" + ")");
+
+console.log("Стоимость разработки сайта " + fullPrice + " рублей" + "(" + Math.round(fullPrice/77.82) + " долларов" + " или " + Math.round(fullPrice/2.7) + " гривен" + " или " + Math.round(fullPrice/12.23) + " юаней" + ")");
+
+console.log(screens.toLowerCase().split(" "));
+console.log("Процент отката посреднику за работу " + (fullPrice * (rollback/100)) + " монгольских тубриков");
+
