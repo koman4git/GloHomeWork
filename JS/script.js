1edab11b0fc2c08a053955eb66c82dab04367eb1
Andrey Lazarew <koman4bk@bk.ru>
2022-02-21 13:02:51 +0300

    поплучение title страницы

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,19 @@
 'use strict';
 const title = document.getElementsByTagName('h1')[0],
-      startBtn = document.getElementsByClassName('.handler_btn')[0],
-      resetBtn = document.getElementsByClassName('.handler_btn')[1],
+      startBtn = document.getElementsByClassName('handler_btn')[0],
+      resetBtn = document.getElementsByClassName('handler_btn')[1],
       screenBtn = document.querySelector('.screen-btn'),
       otherItemsPercent = document.querySelectorAll('.other-items.percent'),
       otherItemsNumber  = document.querySelectorAll('.other-items.number '),
       typeRange = document.querySelector('.rollback  input'),
-      spanRangeValue = document.querySelector('.rollback  span'),
-      totalInput = document.getElementsByClassName('total-input'),
-      totalInputCost = totalInput[0],
-      totalInputTotalCount = totalInput[1],
-      totalInputTotalCountOther = totalInput[2],
-      totalInputTotalFullCount = totalInput[3],
-      totalInputTotalCountRollback = totalInput[4];
+      inputRangeValue = document.querySelector('.rollback  .range-value'),
+      InputCost = document.getElementsByClassName('total-input')[0],
+      TotalCount = document.getElementsByClassName('total-input')[1],
+      TotalCountOther = document.getElementsByClassName('total-input')[2],
+      TotalFullCount = document.getElementsByClassName('total-input')[3],
+      TotalCountRollback = document.getElementsByClassName('total-input')[4];
 
-let screen = document.querySelectorAll('.screen');
+let screens = document.querySelectorAll('.screen');
 
 
 const appData = {
@@ -27,17 +26,32 @@ const appData = {
     allServicePrices: 0,
     servicePercentPrice: 0,
     services: {},
+    init: function () {
+      appData.addTitle();
+      appData.start();
+    },
+    addTitle: function () {
+      document.title = title.textContent;
+    },
+    start: function() {
+      // appData.asking();
+      // appData.addPrices();
+      // appData.getFullPrice();
+      // appData.getServicePercentPrices();
+      // appData.getTitle();
+      // appData.logger();
+      // appData.getRollbackMessage();
+    },
+    isNumber: function (num) {
+      return !isNaN(parseFloat(num)) && isFinite(num) && num != ' ';
+    },
     asking: function () {
-      do {
-        appData.title = prompt("Как называется ваш проект?", 'Калькулятор верстки');
-      } while (!appData.isString(appData.title));
-      
+      appData.title = prompt("Как называется ваш проект?", 'Калькулятор верстки');
+            
       for (let i = 0; i < 2; i++) {
-          let name;
+          let name = prompt("Какие типы экранов нужно разработать?", 'Простые или сложные');
           let price = 0;
-          do {
-            name =  prompt("Какие типы экранов нужно разработать?", 'Простые или сложные');
-          } while (!appData.isString(name));
+          
           do {
             price = prompt("Сколько будет стоить данная работа?", '20000');
           }
@@ -46,16 +60,14 @@ const appData = {
         }
 
         for (let i = 0; i < 2; i++) { 
-          let name;
+          let name =  prompt("Какой дополнительный тип услуги нужен?", 'Метрика, отправка форм');
           let price = 0;
-          do {
-            name =  prompt("Какой дополнительный тип услуги нужен?", 'Метрика, отправка форм');
-          } while (!appData.isString(name));
+          
           do {
             price = prompt("Сколько это будет стоить?", '1000');
           }
           while (!appData.isNumber(price));
-          appData.services[name + ' ' + i] = +price;  
+          appData.services[name] = +price;  
           }
           appData.adaptive = confirm("Нужен ли адаптив на сайте?");
     },
@@ -72,9 +84,6 @@ const appData = {
     },
     isString: function(str) {
       return isNaN(parseFloat(str)) && !isFinite(str) && str != ' ';
-    },
-    isNumber: function (num) {
-      return !isNaN(parseFloat(num)) && isFinite(num) && num != ' ';
     },
     getRollbackMessage: function(price) {
       if (price >= 30000) {
@@ -100,20 +109,11 @@ const appData = {
       console.log(appData.fullPrice);
       console.log(appData.servicePercentPrice);
       console.log(appData.screens);
-      console.log(appData.services);
-    },
-    start: function() {
-      appData.asking();
-      appData.addPrices();
-      appData.getFullPrice();
-      appData.getServicePercentPrices();
-      appData.getTitle();
-      appData.logger();
-      appData.getRollbackMessage();
+      
     }
     
-}
-appData.start();
+};
+appData.init()
     
   
 
